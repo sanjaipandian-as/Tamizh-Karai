152fe838e0f22eec78e5ec4569a141ae2daea11c
Sanjaipandian-as <sanjaipandian.as@gmail.com>
2026-01-08 09:20:59 +0530

    Change API base URL to production server

--- Front-End/API.js
+++ Front-End/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://tamizh-karai-2.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
